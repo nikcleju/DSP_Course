178222aaf1937d60c2c0bff55f58389bfce5a365
Nicolae Cleju <nikcleju@gmail.com>
2021-10-06 11:00:24 +0300

    Removed Work

--- Work/L03_5316_5315/Ex3.m
+++
@@ -1,9 +0,0 @@
-clear all
-close all
-
-R = [ zeros(200,100)   ones(200,100)   ones(200,100)];
-G = [ zeros(200,100)   ones(200,100)   zeros(200,100)];
-B = [ 0.5*ones(200,100)   zeros(200,100)   zeros(200,100)];
-
-Icolor = cat(3, R, G, B);
-imshow(Icolor)
--- Work/L05_5315_5316/Ex1.m
+++
@@ -1,8 +0,0 @@
-
-x = [zeros(1,20)  ones(1,20)];
-x = [x x x x x];   % nice
-
-y = mysys1(x);
-
-plot( [x' y'] )
-legend('Input signal x[n]', 'Output signal y[n]');
